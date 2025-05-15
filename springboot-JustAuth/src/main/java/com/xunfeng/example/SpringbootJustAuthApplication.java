838ec87d951f0d6838c5dd701840b0f39aa3f75c
xiongfeng <xiongfeng@hfitech.com>
2025-05-15 18:33:34 +0800

    init: 项目初始化，模仿quickStart项目尝试gitlab第三方登录，未发现私有gitlab地址配置方式，暂时放弃 https://github.com/justauth/justauth-spring-boot-starter #0

---
+++ springboot-JustAuth/src/main/java/com/xunfeng/example/SpringbootJustAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xunfeng.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJustAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJustAuthApplication.class, args);
+    }
+
+}
